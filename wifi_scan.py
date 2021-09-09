42cccdcd123c01620174270dbcbc4d631a04ab92
Michael Hirsch <scivision@users.noreply.github.com>
2021-09-09 03:47:25 -0400

    add json logging to wifi

--- wifi_scan.py
+++ wifi_scan.py
@@ -9,6 +9,9 @@ import pprint
 import re
 import subprocess
 import argparse
+import datetime
+import json
+from pathlib import Path
 
 
 cellRe = re.compile(r"^Cell\s+(?P<cellnumber>.+)\s+-\s+Address:\s(?P<mac>.+)$")
@@ -57,11 +60,36 @@ def parse(raw: str) -> dict[str, dict[str, str]]:
 
 if __name__ == "__main__":
     P = argparse.ArgumentParser(description="WiFi scanner using iw")
+    P.add_argument("-N", default=10, type=int, help="number of scans")
     P.add_argument("-i", "--interface", default="wlan0", help="WiFi interface to scan")
+    P.add_argument("logpath", help="directory to write JSON output to", default=".")
     args = P.parse_args()
 
-    raw = scan(iface=args.interface)
-    # raw = open("input.txt").read()
+    FMT = "YYYY-MM-DDTHH_MM_SS"
 
-    aps = parse(raw)
-    pprint.pprint(aps)
+    print(f"Wifi scan: {args.N} loops")
+
+    now = datetime.datetime.now().strftime(FMT)
+
+    logfile = Path(args.logpath).expanduser() / ("wifi_" + now + ".json")
+
+    dat_all = {}
+    jstr = ""
+
+    for _ in range(args.N):
+        now = datetime.datetime.now().strftime(FMT)
+
+        raw = scan(iface=args.interface)
+        # raw = open("input.txt").read()
+
+        aps = parse(raw)
+        pprint.pprint(aps)
+
+        if logfile.is_file():
+            jstr = logfile.read_text()
+            dat_all = json.loads(jstr)
+
+        dat_all[now] = aps
+
+        jstr = json.dumps(dat_all)
+        logfile.write_text(jstr)
